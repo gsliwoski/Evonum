442c889032472b94db2fa249e13ecdec7661e2a5
Gregory Sliwoski <gregory.r.sliwoski@vanderbilt.edu>
2015-10-22 19:00:57 -0500

    added comments.

--- evonum_mutations.py
+++ evonum_mutations.py
@@ -4,12 +4,19 @@ import random
 
 
 class Mutations(object):
-
+    """Collection of all potential mutations.
+    
+    May store attribute-based mutations to make solver mutations easier.
+    """
     @staticmethod
     def GaussianMutation(initial, spread):
+        """Simple gaussian-based mutation.
+        
+        Takes center and width and returns new value."""
 #		print ("Gaussian Mutation with initial=%d and spread=%.2f" % (initial, spread))
         return random.gauss(initial, spread)
 
     @staticmethod
     def HardMutation(low, high):
+        """Hard int mutation."""
         return random.randrange(low, high)
